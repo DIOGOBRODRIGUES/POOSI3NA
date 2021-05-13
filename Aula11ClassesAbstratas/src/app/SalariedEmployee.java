23625ee54a07a63861baae6162a443017eea76dd
Diogo Rodrigues <diogo.borbar@gmail.com>
2021-05-13 20:32:55 -0300

    SalariedEmployee

---
+++ Aula11ClassesAbstratas/src/app/SalariedEmployee.java
@@ -0,0 +1,31 @@
+package app;
+
+public class SalariedEmployee extends Employee{
+	private double weeklySalary;
+	
+	public SalariedEmployee(String first, String last, String ssn, double salary) {
+		super(first, last, ssn);
+		this.setWeeklySalary(salary);
+	}
+	
+	public double getWeeklySalary() {
+		return this.weeklySalary;
+	}
+	
+	public void setWeeklySalary(double salary) {
+		/*if(salary < 0)
+			this.weeklySalary = 0;
+		else
+			this.weeklySalary = salary;*/
+		this.weeklySalary = (salary < 0.0) ? 0.0 : salary;
+	}
+	
+	public double earnings() {
+		return getWeeklySalary();
+	}
+	
+	public String toString() {
+		return String.format("salaried employee: %s \n%s: %.2f", super.toString(), 
+				"weekly salary", this.getWeeklySalary());
+	}
+}
