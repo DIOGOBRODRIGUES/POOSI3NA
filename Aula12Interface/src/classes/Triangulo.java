70be74a893d27965ce9690e3ea87f8d4841e830d
Diogo Rodrigues <diogo.borbar@gmail.com>
2021-05-20 19:45:41 -0300

    Interface Triangulo

---
+++ Aula12Interface/src/classes/Triangulo.java
@@ -0,0 +1,28 @@
+package classes;
+
+import interfaces.FiguraGeometrica;
+
+public class Triangulo implements FiguraGeometrica{
+	private String nomeTriangulo;
+	private int ladoA;
+	private int ladoB;
+	private int ladoC;
+	private int base;
+	private int altura;
+	
+	@Override
+	public String getNomeFigura() {
+		return this.nomeTriangulo;
+	}
+	
+	@Override
+	public int getArea() {
+		return (base * altura) / 2;
+	}
+	
+	@Override 
+	public int getPerimetro() {
+		return ladoA + ladoB + ladoC;
+	}
+	
+}
